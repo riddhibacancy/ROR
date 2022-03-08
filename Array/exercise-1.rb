9754b10ebe7f6c4fd7e0150c529b37e3783c6193
Riddhi Mehta <riddhi.mehta@bacancy.com>
2022-03-08 11:13:10 +0530

    Update exercise-1.rb

--- Array/exercise-1.rb
+++ Array/exercise-1.rb
@@ -1,4 +1,6 @@
-arr = [2, 7, 11, 15]
-target = 13
-results = (0...arr.size).to_a.combination(2).select { |first, last| arr[first] + arr[last] == target }
-print results.first #=> [0, 1]
+def array_sum(arr, target)
+  results = (0...arr.size).to_a.combination(2).select { |first, last| arr[first] + arr[last] == target }
+  results.first
+end
+
+array_sum([2,7,11,15], 13)
